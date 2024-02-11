47c3231cc68b6abd4271b976c72b7b2ad568c682
Merilin Pruul <merilin.pruul@tktk.ee>
2024-02-11 20:44:46 +0200

    axios-heroku

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express ();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res)=>{
+    let url = 'https://api.themoviedb.org/3/movie/429617?api_key=f0f67af4e84b5a73497a9274c2bfb78f';
+    axios.get(url)
+    .then(response => {
+        let data = response.data;
+        let releaseDate = new Date(data.release_date).getFullYear();
+
+        let genresToDisplay = '';
+        data.genres.forEach(genre => {
+            genresToDisplay = genresToDisplay + `${genre.name}, `;
+        });
+        let genresUpdated = genresToDisplay.slice(0, -2) + '.';
+
+        let posterUrl = `https://www.themoviedb.org/t/p/w600_and_h900_bestv2${data.poster_path}`;
+
+
+        let currentYear = new Date().getFullYear();
+
+        res.render('index', {
+            dataToRender: data, 
+            year: currentYear,
+            releaseYear: releaseDate,
+            genres: genresUpdated,
+            poster: posterUrl
+        });
+    });
+
+    
+});
+
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log('Server is running on Port 3000');
+});
